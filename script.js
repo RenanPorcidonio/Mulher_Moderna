cd3849b09b8a98cc203df6f6874c2108f41ad6a6
RenanPorcidonio <[porcidoniorenan@gmail.com]>
2021-10-16 22:52:34 -0300

    botões da nav corrigidos

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-var btnVer = document.querySelector('#btn-ver');
-var verMais = document.querySelector('.ver-mais');
-var tituloBlusas = document.querySelector('.titulo-categoria');
-var maisItens = document.querySelector('mais-itens');
+let btnVer = document.querySelector('#btn-ver')
+let verMais = document.querySelector('.ver-mais')
+let maisItens = document.querySelector('.mais-itens')
+let navBtn = document.querySelectorAll(".navBtn")
 
-verMais.addEventListener("click", function change() {
-    if (!btnVer.checked) {
-        verMais.style = "background-color: black; color: white;";
-        verMais.innerText = "Ver menos";
-        tituloBlusas.style.display = "block";
-        maisItens.style.display = "block";   
-    }
-    else {
-        verMais.style = "";
-        tituloBlusas.style.display = "none";
-        verMais.innerText = "Ver mais";
-        maisItens.style.display = "none";
-    }
-})
+function verMaisItens() {
+    maisItens.classList.toggle("toggle-show")
+}
+verMais.addEventListener("click", verMaisItens)
+for (let i = 0; i < navBtn.length; i++) {
+    navBtn[i].addEventListener("click", verMaisItens)
+}
